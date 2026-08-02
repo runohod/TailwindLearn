0600563074766ac8168f1ff0f748ce46d3dc8235
runohod <andrei.rumynthev1728@yandex.ru>
2026-08-02 23:46:11 +0300

    доделана верстка services

--- src/shared/assets/sevices/index.js
+++ src/shared/assets/sevices/index.js
@@ -1,5 +1,5 @@
 export { default as background} from './img/background.jpg';
-export { default as construction} from './img/background.jpg';
-export { default as projectDevelopment} from './img/background.jpg';
-export { default as interiorDesign} from './img/background.jpg';
-export { default as repairs} from './img/background.jpg';
+export { default as construction} from './img/construction.jpg';
+export { default as projectDevelopment} from './img/projectDevelopment.jpg';
+export { default as interiorDesign} from './img/interiorDesign.jpg';
+export { default as repairs} from './img/repairs.jpg';
